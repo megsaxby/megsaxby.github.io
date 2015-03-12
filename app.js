336e606b2a9ae6a2a610382748b6164611c14ad8
queenfrankie <laurawadden@gmail.com>
2015-03-12 22:03:39 +0100

    Basic app setup with Express, Jade and Sass

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require('express')
+var sassMiddleware = require('node-sass-middleware')
+var path = require('path')
+
+var app = express()
+
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jade')
+
+app.use(sassMiddleware({
+    src: path.join(__dirname, 'public'),
+    dest: path.join(__dirname, 'public'),
+    debug: true,
+    outputStyle: 'compressed',
+    prefix:  '/prefix'
+}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function (req, res) {
+  res.render('index', 
+  	{ title : 'Home'}
+  )
+})
+app.listen(3000)
