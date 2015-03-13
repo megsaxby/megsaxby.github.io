f4221b12c78519e92131fc4951750cc87e5a3d62
queenfrankie <laurawadden@gmail.com>
2015-03-13 18:41:58 +0100

    basic layout of homepage and background image

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(sassMiddleware({
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
-  res.render('index', 
+  res.render('home', 
   	{ title : 'Home'}
   )
 })
